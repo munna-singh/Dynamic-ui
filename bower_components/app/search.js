18e8cee29c35b7f7cef7a6fd8407af1f1a0d7e6e
Munna Singh <dreammunna@yahoo.co.in>
2017-10-27 20:20:13 +0530

    Google Search implemented

--- bower_components/app/search.js
+++ bower_components/app/search.js
@@ -52,11 +52,11 @@ $(document)
 
             initialize: function() {
                 this.initInputForm();
-                this.findAddress();
 				this.getQuotes();
             },
 
             initInputForm: function() {
+				var self= this;
                 $('select').select2();
                 $('.activitydate')
                     .datepicker({
@@ -71,80 +71,91 @@ $(document)
                         });
                 $("#startdate").datepicker('setDate', moment().add(1, 'months').toDate());
                 $("#enddate").datepicker('setDate', moment().add(3, 'days').add(1, 'month').toDate());
-            },
+				
+				try {
+                    autocomplete = new google.maps.places.Autocomplete(this.$('.destination-typeahead')[0]);
 
-            findAddress: function() {
-                var map = {};
-                $('[id*=destination]')
-                    .typeahead({
-                        hint: true,
-                        highlight: true,
-                        minLength: 3,
-                        source: function(request, response) {
-                            var items = [];
-                            var data = $.grep(airports,
-                                function(airport) {
-                                    airport = airport.properties;
-                                    if (request.length === 3)
-                                        return airport.IATACode.toLowerCase() === request.toLowerCase() ||
-                                            airport.IATACityCode.toLowerCase() === request.toLowerCase() ||
-                                            airport.MACCode.toLowerCase() === request.toLowerCase();
-                                    else {
-                                        var spaceterm = request;
-                                        return airport.Name.toLowerCase().startsWith(spaceterm) ||
-                                            airport.Name.toLowerCase().indexOf(spaceterm.toLowerCase()) !== -1 ||
-                                            airport.IATACode.toLowerCase().startsWith(spaceterm) ||
-                                            airport.IATACityCode.toLowerCase().startsWith(spaceterm) ||
-                                            airport.MACCode.toLowerCase().startsWith(spaceterm);
-                                    }
-                                });
-                            $.each(data,
-                                function(i, item) {
-                                    item = item.properties;
-                                    var id = item.IATACode;
-                                    var name = id + " - " + item.Name;
-                                    map[name] = { id: id, name: id + " " + name, lat: item.Latititude, lon: item.Longitude };
-                                    items.push(name);
-                                });
-                            response(items);
-                            $(".dropdown-menu").css("height", "auto");
-                        },
-                        updater: function(item) {
-                            $('[id*=IATACode]').val(map[item].id);
-                            $('[id*=Latitude]').val(map[item].lat);
-                            $('[id*=Longitude]').val(map[item].lon);
-                            return item;
+                    autocomplete.addListener('place_changed', function () {
+                        var place, location, state, country;
+                        place = autocomplete.getPlace();
+                        if (!place.geometry) {
+                            window.alert("Information not returned by google.");
+                            return;
                         }
+						self.model = new Backbone.Model();
+						location = _.find(place.address_components, function(addr){return addr.types.indexOf("locality") > -1; });
+						state = _.find(place.address_components, function(addr){return addr.types.indexOf("administrative_area_level_1") > -1; });
+						country = _.find(place.address_components, function(addr){return addr.types.indexOf("country") > -1; });
+						 
+                        self.model.set({
+                            Destination: {
+                                FormattedName: place.formatted_address,
+                                DestinationType: 2,//EnumTypes.HotelSearchTypeTypes.GeoLocation
+								Name:location?location.long_name:null,
+								State:state?state.long_name:null,
+								Country:country?country.long_name:null
+                            },
+                            Location: {
+                                Latitude: place.geometry.location.lat(),
+                                Longitude: place.geometry.location.lng()
+                            }
+                        });
                     });
+                } catch (e) {//ignore jslint
+                    // Ignore this.
+                }
+				
             },
 
+			getSelectedAgents: function (mainkey, subkey) {
+                var name = mainkey + "=";
+				var subCookie = subkey + "=";
+				
+				var firstString;
+				var ca = document.cookie.split(';');
+				for(var i = 0; i < ca.length; i++) {
+					var c = ca[i];
+					while (c.charAt(0) == ' ') {
+						c = c.substring(1);
+					}
+					if (c.indexOf(name) == 0) {
+						firstString =  c.substring(name.length, c.length);
+						break;
+					}
+				}
+				if(firstString){
+					var cookieName = firstString.split('&');
+					for(var k = 0; k < ca.length; k++){
+						var split = cookieName[k];
+						if(split.indexOf(subCookie) === 0){
+							return split.substring(subCookie.length, split.length);
+						}
+					}
+				}
+				return "";
+            },
+			
             doAvailabilitySearch: function(e) {
                 var template,
                     dataObj,
                     self = this;
                 dataObj = this.$('#RegMetadata').serializeObject();
                 var dataInput = {
-                    "Destination":
-                    {
-                        "IATACode": dataObj.IATACode,
-                        "DestinationType": "1",
-                        "Description": $("#destination").val(),
-                        "Latitude": dataObj.Latitude,
-                        "Longitude": dataObj.Longitude
-                    },
+                    "Destination": self.model.get("Destination"),
+					"Location": self.model.get("Location"),
                     "ActivityType": dataObj.ActivityType,
                     "StartDate": dataObj.StartDate,
                     "EndDate": dataObj.EndDate,
                     "MinRating": dataObj.MinRating,
                     "MaxRating": dataObj.MaxRating,
-                    "AgentId": 1347,
+                    "AgentId": self.getSelectedAgents("user","AgentId"),
                     "Currency": "USD",
                     "ProviderType": "1",
                     "QuoteOwners": [
                         {
                             "DelegatorFirstName": "System",
                             "DelegatorLastName": "Kensingtontours ",
-                            "DelegateAgentId": 1347,
+                            "DelegateAgentId": self.getSelectedAgents("user","AgentId"),
                             "IsUser": true,
                             "Selected": true
                         }
